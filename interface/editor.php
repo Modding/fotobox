7faf03dd8d5278a756125fe8df6eb7ad3139ac77
Modding <valentin@seipt.org>
2020-06-05 21:25:45 +0200

    editor.php addet

---
+++ interface/editor.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$file = $_GET['file'];
+
+    switch ($file){
+        case 1:
+            $edit = "../config.py";
+            break;
+        case 2:
+            $edit = "../design/template.html";
+            break;
+        default:
+            die('No file given');
+    }
+// configuration
+$url = '/edit.php?file=' . $file;
+$edit = '/path/to/txt/file';
+
+// check if form has been submitted
+if (isset($_POST['text']))
+{
+    // save the text contents
+    file_put_contents($edit, $_POST['text']);
+
+    // redirect to form again
+    header(sprintf('Location: %s', $url));
+    printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
+    exit();
+}
+
+// read the textfile
+$text = file_get_contents($edit);
+
+?>
+<!-- HTML form -->
+<form action="" method="post">
+    <textarea name="text"><?php echo htmlspecialchars($text) ?></textarea>
+    <input type="submit" />
+    <input type="reset" />
+</form>
+
+
