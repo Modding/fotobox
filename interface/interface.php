1cef6e65bbf645681c51239e4cfe2035c583a1d2
Modding <valentin@seipt.org>
2020-06-06 01:19:45 +0200

    Interface gebaut

--- interface/interface.php
+++ interface/interface.php
@@ -1,3 +1,21 @@
 <?php
 include "session.php";
-echo $_SESSION["user"];
+?>
+
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>Hässliches Admin Interface</h1><br>
+        <h2>File Editor:</h2><br>
+            <a href="editor.php?file=1" target="_blank">Edit config.py</a><br>
+            <a href="editor.php?file=2" target="_blank">Edit template.html</a><br>
+            <a href="editor.php?file=3" target="_blank">Edit fotobox.py</a><br>
+        <h2>Dateimanager:</h2><br>
+            <a href="tinyfilemanager.php" target="_blank">Template Files</a><br>
+        <h2>Admin Tasks:</h2><br>
+            <a href="update.php" target="_blank">Git update</a><br>
+            <a href="reboot.php" target="_blank">Reboot Fotobox</a><br>
+    </body>
+</html>
