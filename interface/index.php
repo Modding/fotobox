eb7d1b5f5c2147157e9728267ad62da2bbeedade
Modding <valentin@seipt.org>
2020-06-05 23:58:06 +0200

    pw hash

--- interface/index.php
+++ interface/index.php
@@ -1,2 +1,10 @@
 <?php
-phpinfo();
+
+include_once "config.php";
+
+function hashmypw($pw, $salt){
+    $hash = hash(sha512, $pw. $salt);
+    return $hash;
+}
+
+echo hashmypw($_GET['pw'], $user['salt']);
