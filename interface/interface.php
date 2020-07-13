863aae8360486a7b82e0b06b97a5a93a2b50b069
Modding <valentin@seipt.org>
2020-07-13 16:48:33 +0200

    addet sync.sh to interface

--- interface/interface.php
+++ interface/interface.php
@@ -12,6 +12,7 @@ include "session.php";
             <a href="editor.php?file=1" target="_blank">Edit config.py</a><br>
             <a href="editor.php?file=2" target="_blank">Edit template.html</a><br>
             <a href="editor.php?file=3" target="_blank">Edit fotobox.py</a><br>
+            <a href="editor.php?file=4" target="_blank">Edit sync.sh</a><br>
         <h2>Dateimanager:</h2><br>
             <a href="tinyfilemanager.php" target="_blank">Template Files</a><br>
         <h2>Admin Tasks:</h2><br>
